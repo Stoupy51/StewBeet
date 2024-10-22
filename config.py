eb65468ebc41f98b67aa55daa6209fab4aa81245
Stoupy51 <stoupy51@gmail.com>
2024-10-22 21:06:50 +0200

    ♻️ Synchronized with latest package version

--- config.py
+++ config.py
@@ -8,8 +8,7 @@ IGNORE_UNSET: bool = False							# If True, the program will ignore unset option
 # Folders
 MERGE_FOLDER: str = f"{ROOT}/merge"					# If a file exists in both merge and build folder, they will be merged. Otherwise, it's just copied.
 BUILD_FOLDER: str = f"{ROOT}/build"					# Folder where the final datapack and resource pack are built
-ASSETS_FOLDER: str = f"{ROOT}/assets"				# Folder containing the all assets (textures, sounds, ...) for the datapack
-TEXTURES_FOLDER: str = f"{ASSETS_FOLDER}/textures"	# Folder containing the textures for the datapack
+ASSETS_FOLDER: str = f"{ROOT}/assets"				# Folder containing the all assets (textures, sounds, ... folders) for the datapack and resource pack
 LIBS_FOLDER: str = f"{ROOT}/libs"					# The libraries are copied to the build destination, and merged with the datapack using Weld
 BUILD_COPY_DESTINATIONS: tuple[list, list] = (["D:/latest_snapshot/world/datapacks"], ["D:/minecraft/snapshot/resourcepacks"])	# Can be empty lists if you don't want to copy the generated files to other folders.
 
@@ -25,12 +24,8 @@ MERGE_LIBS: bool = True								# Make new zip of merged libraries with the datap
 # Datapack related constants
 AUTHOR: str = "Stoupy51"				# Author(s) name(s) displayed in pack.mcmeta, also used to add convention.debug tag to the players of the same name(s) <-- showing additionnal displays like datapack loading
 DATAPACK_NAME: str = "Template"			# Name of the datapack, used for messages and items lore
-MINECRAFT_VERSION: str = "1.21"			# Text used when loading the datapack to warn the user when the data version is not right
-DATA_VERSION: int = 3947				# Depending on MC version, given by /data get entity @p DataVersion to check if the datapack is not running in an older version of MC
 VERSION: str = "1.21.615"				# Datapack version in the following mandatory format: major.minor.patch, ex: 1.0.0 or 1.21.615
 NAMESPACE: str = "your_namespace"		# Simplified version of the datapack name. Should be the same you use in the merge folder. Used to namespace functions, tags, etc.
-DATAPACK_FORMAT: int = 48				# Pack format version, see https://minecraft.wiki/w/Pack_format#List_of_data_pack_formats
-RESOURCE_PACK_FORMAT: int = 34			# Resource pack format version, see https://minecraft.wiki/w/Pack_format#List_of_resource_pack_formats
 DESCRIPTION = f"{DATAPACK_NAME} [{VERSION}] by {AUTHOR}"	# Pack description displayed in pack.mcmeta
 DEPENDENCIES: dict[str, dict[str, list[int] | str]] = {
 	# Automagically, the datapack will check for the presence of dependencies and their minimum required versions at runtime
@@ -73,7 +68,6 @@ configuration = {
 	"manual_path": MANUAL_PATH,
 	"build_folder": BUILD_FOLDER,
 	"assets_folder": ASSETS_FOLDER,
-	"textures_folder": TEXTURES_FOLDER,
 	"libs_folder": LIBS_FOLDER,
 	"build_copy_destinations": BUILD_COPY_DESTINATIONS,
 	"has_manual": HAS_MANUAL,
@@ -87,12 +81,8 @@ configuration = {
 	"merge_libs": MERGE_LIBS,
 	"author": AUTHOR,
 	"datapack_name": DATAPACK_NAME,
-	"minecraft_version": MINECRAFT_VERSION,
-	"data_version": DATA_VERSION,
 	"version": VERSION,
 	"namespace": NAMESPACE,
-	"datapack_format": DATAPACK_FORMAT,
-	"resource_pack_format": RESOURCE_PACK_FORMAT,
 	"manual_name": MANUAL_NAME,
 	"description": DESCRIPTION,
 	"dependencies": DEPENDENCIES,
