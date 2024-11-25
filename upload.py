642a2da3a242cf285ffd6be5dc48b3b80ef5b44f
Stoupy51 <stoupy51@gmail.com>
2024-11-25 10:25:38 +0100

    feat: ✨ Added github and pmc upload scripts

---
+++ upload.py
@@ -0,0 +1,16 @@
+
+# Imports
+from python_datapack.continuous_delivery import load_credentials, upload_to_github, upload_to_modrinth, upload_to_smithed, upload_to_pmc
+
+# Get credentials
+credentials: dict[str, str] = load_credentials("~/python_datapack/credentials.yml")
+
+## Uploads
+# Upload to GitHub
+from continuous_delivery.github_config import github_config
+changelog: str = upload_to_github(credentials, github_config)
+
+# Upload to PlanetMinecraft
+from continuous_delivery.pmc_config import pmc_config
+upload_to_pmc(pmc_config, changelog)
+
