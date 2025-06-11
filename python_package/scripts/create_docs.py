8c37cce9a35ada8421d82069a9bb1d27657f132c
Stoupy51 <stoupy51@gmail.com>
2025-06-11 21:09:58 +0200

    feat: ✨ Add new scripts for documentation generation, GitHub release, and PyPI routine

---
+++ python_package/scripts/create_docs.py
@@ -0,0 +1,33 @@
+
+# Imports
+import sys
+
+import stouputils.applications.automatic_docs as app
+from stouputils.io import get_root_path
+
+# Update documentation
+if __name__ == "__main__":
+
+	version: str | None = None
+	if len(sys.argv) == 2:
+		version = sys.argv[1]
+	elif len(sys.argv) == 1:
+		pass
+	else:
+		raise ValueError("Usage: python create_docs.py [version]")
+
+	# Update documentation
+	app.update_documentation(
+		root_path=get_root_path(__file__, go_up=2),
+		project="StewBeet",
+		author="Stoupy",
+		copyright="2025, Stoupy",
+		html_logo="https://raw.githubusercontent.com/Stoupy51/StewBeet/refs/heads/main/templates/extensive/assets/stewbeet_1024x1024.png",
+		html_favicon="https://raw.githubusercontent.com/Stoupy51/StewBeet/refs/heads/main/templates/extensive/assets/stewbeet_1024x1024.png",
+		github_user="Stoupy51",
+		github_repo="stewbeet",
+		html_theme="pydata_sphinx_theme",
+		version=version,
+		skip_undocumented=True,
+	)
+
