a0b22d8aa8932261b65d416d3c67afbc05a16a4f
Stoupy51 <stoupy51@gmail.com>
2025-06-14 16:52:38 +0200

    build: 🚀 Update ItemIO version to 1.4.0

--- python_package/stewbeet/core/constants.py
+++ python_package/stewbeet/core/constants.py
@@ -94,7 +94,7 @@ OFFICIAL_LIBS: dict[str, dict] = {
 	"smithed.crafter":		{"version":[0, 7, 0], "name":"Smithed Crafter",					"url":"https://wiki.smithed.dev/libraries/crafter/",		"is_used": False},
 	"furnace_nbt_recipes":	{"version":[1, 9, 0], "name":"Furnace NBT Recipes",				"url":"https://github.com/Stoupy51/FurnaceNbtRecipes",		"is_used": False},
 	"smart_ore_generation":	{"version":[1, 7, 1], "name":"SmartOreGeneration",				"url":"https://github.com/Stoupy51/SmartOreGeneration",		"is_used": False},
-	"itemio":				{"version":[1, 3, 3], "name":"ItemIO",							"url":"https://github.com/edayot/ItemIO",					"is_used": False},
+	"itemio":				{"version":[1, 4, 0], "name":"ItemIO",							"url":"https://github.com/edayot/ItemIO",					"is_used": False},
 	**BOOKSHELF_MODULES,
 }
 
