fab36490136e845fd63d8bd5f4a4b49d4b271bc9
Stoupy51 <stoupy51@gmail.com>
2025-07-01 15:26:01 +0200

    build: 🚀 Updated official dependencies

--- python_package/stewbeet/core/constants.py
+++ python_package/stewbeet/core/constants.py
@@ -90,12 +90,12 @@ def official_lib_used(lib: str) -> bool:
 	return is_used
 
 OFFICIAL_LIBS: dict[str, dict] = {
-	"common_signals":		{"version":[0, 1, 0], "name":"Common Signals",					"url":"https://github.com/Stoupy51/CommonSignals",			"is_used": False},
-	"smithed.custom_block":	{"version":[0, 7, 0], "name":"Smithed Custom Block",			"url":"https://wiki.smithed.dev/libraries/custom-block/",	"is_used": False},
-	"smithed.crafter":		{"version":[0, 7, 0], "name":"Smithed Crafter",					"url":"https://wiki.smithed.dev/libraries/crafter/",		"is_used": False},
-	"furnace_nbt_recipes":	{"version":[1, 9, 0], "name":"Furnace NBT Recipes",				"url":"https://github.com/Stoupy51/FurnaceNbtRecipes",		"is_used": False},
-	"smart_ore_generation":	{"version":[1, 7, 1], "name":"SmartOreGeneration",				"url":"https://github.com/Stoupy51/SmartOreGeneration",		"is_used": False},
-	"itemio":				{"version":[1, 4, 0], "name":"ItemIO",							"url":"https://github.com/edayot/ItemIO",					"is_used": False},
+	"common_signals":		{"version":[0, 1, 1], "name":"Common Signals",					"url":"https://github.com/Stoupy51/CommonSignals",			"is_used": False},
+	"smithed.custom_block":	{"version":[0, 7, 1], "name":"Smithed Custom Block",			"url":"https://wiki.smithed.dev/libraries/custom-block/",	"is_used": False},
+	"smithed.crafter":		{"version":[0, 7, 1], "name":"Smithed Crafter",					"url":"https://wiki.smithed.dev/libraries/crafter/",		"is_used": False},
+	"furnace_nbt_recipes":	{"version":[1, 9, 1], "name":"Furnace NBT Recipes",				"url":"https://github.com/Stoupy51/FurnaceNbtRecipes",		"is_used": False},
+	"smart_ore_generation":	{"version":[1, 7, 2], "name":"SmartOreGeneration",				"url":"https://github.com/Stoupy51/SmartOreGeneration",		"is_used": False},
+	"itemio":				{"version":[1, 4, 1], "name":"ItemIO",							"url":"https://github.com/edayot/ItemIO",					"is_used": False},
 	**BOOKSHELF_MODULES,
 }
 
