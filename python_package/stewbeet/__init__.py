976f7b93efbd2f57097ec85d4b6836207079cb49
Stoupy51 <stoupy51@gmail.com>
2025-07-02 16:12:28 +0200

    feat: ✨ Add all plugins support and refactor initialization in StewBeet

--- python_package/stewbeet/__init__.py
+++ python_package/stewbeet/__init__.py
@@ -9,3 +9,8 @@ from .plugins.initialize.source_lore_font import find_pack_png
 from .plugins.resource_pack.item_models.object import AutoModel
 from .plugins.resource_pack.sounds import add_sound
 
+
+def beet_default(ctx: Context):
+    """ Initializes the StewBeet package. """
+    ctx.require("stewbeet.plugins.initialize")
+
