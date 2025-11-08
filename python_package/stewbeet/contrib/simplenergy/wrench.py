9c6a8513c1e1f3233e06165fff05dc1851d7aa7f
Stoupy51 <stoupy51@gmail.com>
2025-11-08 12:18:41 +0100

    feat(contrib): ✨ Enhance setup_wrench function from SimplEnergy

--- python_package/stewbeet/contrib/simplenergy/wrench.py
+++ python_package/stewbeet/contrib/simplenergy/wrench.py
@@ -3,23 +3,26 @@
 # Imports
 from beet import FunctionTag
 
-from ...core import Mem, set_json_encoder, write_function
+from ...core import VANILLA_BLOCK, Mem, set_json_encoder, write_function
 
 
 # Setup simplenergy wrench rotatable tags and mechanization calls
-def setup_wrench(blocks: list[str]) -> None:
+def setup_wrench(blocks: list[str] | str, tag_ns: str = "simplenergy") -> None:
 	""" Setup rotatable tags for blocks and mechanization wrench calls.
 
 	Args:
 		blocks (list[str]): List of block names that should be rotatable. (e.g. ["furnace_generator", "electric_furnace", "electric_smelter", "pulverizer"])
+		tag_ns (str): Namespace for the tags. Default is "simplenergy".
 	"""
 	ns: str = Mem.ctx.project_id
+	if isinstance(blocks, str):
+		blocks = [x for x, y in Mem.definitions.items() if y.get(VANILLA_BLOCK, {}).get("apply_facing") is True]
 
 	# Add tags for rotatables
 	for rotatable in blocks:
 		write_function(
 			f"{ns}:custom_blocks/{rotatable}/place_secondary",
-			f"\n# Make the block rotatable by wrench\ntag @s add {ns}.rotatable"
+			f"\n# Make the block rotatable by wrench\ntag @s add {tag_ns}.rotatable"
 		)
 
 
@@ -37,6 +40,6 @@ execute if entity @s[tag={ns}.custom_block] run function {ns}:custom_blocks/dest
 """)
 	write_function(
 		f"{ns}:calls/mechanization/wrench_modify",
-		f"execute if entity @s[tag={ns}.rotatable] run function {ns}:utils/wrench/rotate"
+		f"execute if entity @s[tag={tag_ns}.rotatable] run function {tag_ns}:utils/wrench/rotate"
 	)
 
