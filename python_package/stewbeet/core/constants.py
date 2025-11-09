b7279474f268e71ae9c597aaf34d5cc4a0fbdbf4
Stoupy51 <stoupy51@gmail.com>
2025-11-09 14:59:31 +0100

    feat(custom_blocks): ✨ Update version for Common Signals and enhance destruction function integration

--- python_package/stewbeet/core/constants.py
+++ python_package/stewbeet/core/constants.py
@@ -143,7 +143,7 @@ def official_lib_used(lib: str) -> bool:
 	return is_used
 
 OFFICIAL_LIBS: dict[str, JsonDict] = {
-	"common_signals":		{"version":[0, 1, 1], "name":"Common Signals",					"url":"https://github.com/Stoupy51/CommonSignals",			"is_used": False},
+	"common_signals":		{"version":[0, 2, 0], "name":"Common Signals",					"url":"https://github.com/Stoupy51/CommonSignals",			"is_used": False},
 	"smithed.custom_block":	{"version":[0, 7, 1], "name":"Smithed Custom Block",			"url":"https://wiki.smithed.dev/libraries/custom-block/",	"is_used": False},
 	"smithed.crafter":		{"version":[0, 7, 1], "name":"Smithed Crafter",					"url":"https://wiki.smithed.dev/libraries/crafter/",		"is_used": False},
 	"furnace_nbt_recipes":	{"version":[1, 9, 1], "name":"Furnace NBT Recipes",				"url":"https://github.com/Stoupy51/FurnaceNbtRecipes",		"is_used": False},
