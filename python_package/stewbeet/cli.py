25b5bf3736de6995cb95bfa7e8e29c84989683fa
Stoupy51 <stoupy51@gmail.com>
2025-12-23 17:25:57 +0100

    fix: 🐛 Fix command cli (ImportError)

--- python_package/stewbeet/cli.py
+++ python_package/stewbeet/cli.py
@@ -7,21 +7,20 @@ import subprocess
 import sys
 
 from beet import ProjectConfig
-from stouputils.decorators import LogLevels, handle_error
-from stouputils.print import GREEN, RED, info, show_version
+from stouputils import GREEN, RED, LogLevels, handle_error, info, show_version
 
 from .utils import get_project_config
 
 
 @handle_error(message="Error while running 'stewbeet'")
 def main():
-    second_arg: str = sys.argv[1].lower() if len(sys.argv) == 2 else ""
+    second_arg: str = sys.argv[1].lower() if len(sys.argv) >= 2 else ""
     if second_arg == "" and len(sys.argv) == 1:
         sys.argv.append("build")
 
     # Print the version of stewbeet, beet, bolt, mecha, and stouputils
     if second_arg in ("--version", "-v", "version"):
-        return show_version("stewbeet", primary_color=RED, secondary_color=GREEN)
+        return show_version("stewbeet", primary_color=RED, secondary_color=GREEN, max_depth=int(sys.argv[-1]) if len(sys.argv) == 3 else 2)
 
     # Try to find and load the beet configuration file
     cfg: ProjectConfig = get_project_config()
