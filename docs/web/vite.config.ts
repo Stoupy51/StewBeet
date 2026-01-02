fd6e160c338be07620e93b6d83bf54e79605409d
Stoupy51 <stoupy51@gmail.com>
2026-01-02 20:16:13 +0100

    docs: 📝 Fix vite config

--- docs/web/vite.config.ts
+++ docs/web/vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5174,
+    port: 4173,
+  },
+  preview: {
+    allowedHosts: ['stewbeet.paralya.fr'],
   }
 })
