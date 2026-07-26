d89542a6a2b4c1d7faefe5a0b78eb9584586e0d2
Stoupy51 <stoupy51@gmail.com>
2026-07-26 20:54:00 +0200

    fix: 🐛 Fixed auto lang silent plugin

--- python_package/stewbeet/plugins/auto/lang_file/silent.py
+++ python_package/stewbeet/plugins/auto/lang_file/silent.py
@@ -7,5 +7,5 @@ from beet import Context
 # Silent mode entry point
 def beet_default(ctx: Context) -> None:
 	from .__init__ import beet_default
-	return stp.silent(beet_default)(ctx, desc="")
+	return stp.silent(beet_default)(ctx)
 
