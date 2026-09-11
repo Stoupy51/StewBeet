ccdbca4dc7608a26db8b54df992d3c285ccd600a
Stoupy51 <stoupy51@gmail.com>
2026-09-11 23:12:40 +0200

    chore(specs): 🔥 Stop tracking specs folder

--- specs/001-stewbeet-vscode-dx/contracts/reference/generated/pack/data/ns/function/hit.mcfunction
+++
@@ -1,6 +0,0 @@
-say hit!
-damage @s 4 minecraft:generic
-effect clear @s minecraft:slowness
-effect give @s minecraft:slowness 3 0
-function ns:nested/aura
-## sourceMappingURL=hit.mcfunction.map
--- specs/001-stewbeet-vscode-dx/contracts/reference/source/spawn.ts
+++
@@ -1,8 +0,0 @@
-// Fictional TypeScript datapack DSL. See ../../README.md.
-import {aura} from 'sniffer-example-runtime';
-
-/** Run once on every entity the pack spawns. */
-export function onSpawn(): void {
-    aura();
-    aura.tick('ns.spawned');
-}
