8bf0736ac9709cf9a51099d58e114bf5b984184f
Stoupy51 <stoupy51@gmail.com>
2026-09-15 21:28:21 +0200

    feat: ✨ Added support for 26.3

--- python_package/stewbeet/core/constants.py
+++ python_package/stewbeet/core/constants.py
@@ -43,6 +43,7 @@ MORE_DATA_PACK_FORMATS: dict[tuple[int, ...], FormatSpecifier] = {
 	(26, 1, 1): (101, 1),
 	(26, 1, 2): (101, 1),
 	(26, 2): (107, 1),
+	(26, 3): (121, 0),
 
 	(9999, 0, 0): (9999, 0),
 }
@@ -79,6 +80,7 @@ MORE_ASSETS_PACK_FORMATS: dict[tuple[int, ...], FormatSpecifier] = {
 	(26, 1, 1): (84, 0),
 	(26, 1, 2): (84, 0),
 	(26, 2): (88, 0),
+	(26, 3): (97, 1),
 
 	(9999, 0, 0): (9999, 0),
 }
@@ -102,6 +104,7 @@ MORE_DATA_VERSIONS: dict[tuple[int, ...], int] = {
 	(26, 1, 1): 4788,
 	(26, 1, 2): 4790,
 	(26, 2): 4903,
+	(26, 3): 5023,
 }
 LATEST_MC_VERSION: str = ".".join(str(x) for x in list(MORE_DATA_VERSIONS.keys())[-1])
 base.LATEST_MINECRAFT_VERSION = LATEST_MC_VERSION
